a5f2b99c2a88b7af135453c66a2e1d45f6dec220
廖东明 <991192001@qq.com>
2016-10-11 12:33:35 +0800

    Merge branch 'lambda' of http://git.oschina.net/liao123abc/JavaNewFeature
    # Conflicts:
    #	app/src/main/java/com/example/liaodongming/javanewfeature/Lambda/OracleEx/Person.java

--- app/src/main/java/com/example/liaodongming/javanewfeature/Lambda/OracleEx/Person.java
+++ app/src/main/java/com/example/liaodongming/javanewfeature/Lambda/OracleEx/Person.java
@@ -2,13 +2,31 @@ package com.example.liaodongming.javanewfeature.Lambda.OracleEx;
 
 /**
  * Created by liaodongming on 2016/9/12.
+ *
+ * http://www.oracle.com/webfolder/technetwork/tutorials/obe/java/Lambda-QuickStart/index.html#section2
+ * https://docs.oracle.com/javase/tutorial/java/javaOO/lambdaexpressions.html
  */
-
 public class Person {
     public enum Sex {
         MALE, FEMALI
     }
 
     String name;
+    //now in 2016, you can sort of use Java 8 language features in Android development but not use the java.time classes
     //LocalDate birthday;
+    String birthday;
+    Sex gender;
+    String emailAddress;
+
+    public int getAge() {
+        return 0;
+    }
+
+    public void printPerson() {
+        //
+    }
+
+    public Sex getGender() {
+        return gender;
+    }
 }
